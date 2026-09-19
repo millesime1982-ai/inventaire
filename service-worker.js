bb67c80b719590de8982906e53ddc77fe01e8299
millesime1982-ai <millesime1982@gmail.com>
2026-09-19 10:27:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'inventaire-v217-pwa-1';
+const CACHE = 'inventaire-v219-pwa-1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
